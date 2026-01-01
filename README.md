aaf82e67496ac1b321cb48de1773f2ec13cfc949
gusgus5000 <aidenfolkman@gmail.com>
2025-12-31 22:52:45 -0700

    add lots of stuff but still need to finish landing page

---
+++ README.md
@@ -0,0 +1,24 @@
+# Use Tailwind 
+
+## we need it its very good
+
+# Artist that i have not used
+
+## James Rodriguez
+
+## Sarah Kim
+
+## Marcus Thompson
+
+## Elena Martinez
+
+## David Park
+
+
+
+
+
+
+
+
+## use command shift v to open better
---
+++ js-folder/landing-page.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+
+const hiddenElements = document.querySelectorAll('.art-sample-grid-container');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
